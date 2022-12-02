3905c8fd92fa7cda543be6d3887de17e93844e3e
YP159 <119686219+YP159@users.noreply.github.com>
2022-12-02 15:27:25 +0000

    Add files via upload

---
+++ SOB29.py
@@ -0,0 +1,57 @@
+n=5
+for i in range(n):
+    for j in range(i+1):
+        if (j==i):
+            print('*',end=' ')
+        else:
+           print('',end=' ')
+
+
+    print()
+             
+for i in range(n-1):
+    for j in range(i,n-1):
+        if (j==(n-1)-1):
+            print('*',end=' ')
+        else:
+            print('',end=' ')
+
+    print()
+
+for i in range(n):
+    for j in range(i+1):
+        if (j==i):
+            print('*',end=' ')
+        else:
+           print('',end=' ')
+
+
+    print()
+             
+for i in range(n-1):
+    for j in range(i,n-1):
+        if (j==(n-1)-1):
+            print('*',end=' ')
+        else:
+            print('',end=' ')
+
+    print()
+
+for i in range(n):
+    for j in range(i+1):
+        if (j==i):
+            print('*',end=' ')
+        else:
+           print('',end=' ')
+
+
+    print()
+             
+for i in range(n-1):
+    for j in range(i,n-1):
+        if (j==(n-1)-1):
+            print('*',end=' ')
+        else:
+            print('',end=' ')
+
+    print()
